0b6f1fd2348bb673da59bef86adf66ae9641557b
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2020-09-14 19:35:50 -0600

    internal/server: fix broken handlers test after some API changes

--- internal/server/handlers_test.go
+++ internal/server/handlers_test.go
@@ -45,7 +45,7 @@ type body struct {
 func TestAPI_create(t *testing.T) {
 	api := New(&mockRepo{})
 
-	req, err := http.NewRequest("POST", "/api/v1/urls", bytes.NewBuffer([]byte(`{ "long": "https://www.reddit.com/r/golang/comments/is81vi/just_because_you_can_doesnt_mean_you_should" }`)))
+	req, err := http.NewRequest("POST", "/new", bytes.NewBuffer([]byte(`{ "longURL": "https://www.reddit.com/r/golang/comments/is81vi/just_because_you_can_doesnt_mean_you_should" }`)))
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -60,7 +60,7 @@ func TestAPI_create(t *testing.T) {
 		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
 	}
 	// `{ "shortURL\": "043fdd58" }`
-	expected := []byte{34, 123, 32, 92, 34, 115, 104, 111, 114, 116, 85, 82, 76, 92, 34, 58, 32, 48, 52, 51, 102, 100, 100, 53, 56, 32, 125, 34, 10}
+	expected := []byte{34, 123, 32, 92, 34, 115, 104, 111, 114, 116, 85, 82, 76, 92, 34, 58, 32, 104, 116, 116, 112, 58, 47, 47, 108, 111, 99, 97, 108, 104, 111, 115, 116, 58, 52, 48, 48, 48, 47, 48, 52, 51, 102, 100, 100, 53, 56, 32, 125, 34, 10}
 	if !bytes.Equal(rr.Body.Bytes(), expected) {
 		t.Errorf("handler returned unexpected body: got %v want %v", rr.Body.Bytes(), expected)
 	}
